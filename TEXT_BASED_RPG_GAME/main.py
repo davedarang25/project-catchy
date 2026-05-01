bd7029c1ef0ea1d9439e974f1455848600c184a9
DaveDarang25 <davedarang2017@gmal.com>
2026-05-01 20:34:06 +0800

    0.0.8

--- TEXT_BASED_RPG_GAME/main.py
+++ TEXT_BASED_RPG_GAME/main.py
@@ -1,1 +1,21 @@
 # main py
+# start coding by Clark David Darang 1 May 2026, 8:30pm
+import sys
+from menu  import show_menu
+from gameplay import start_game
+from leaderboard import show_leaderboard
+
+def main():
+    print("=== Welcome to Exiled Below ===")
+    print("A Text-Based RPG Game developed by Dos Productions")
+    while True:
+        choice = show_menu()
+
+        if choice == '1':
+            start_game()
+
+        elif choice == '2':
+            show_leaderboard()
+
+        elif choice == '3':
+            print()
