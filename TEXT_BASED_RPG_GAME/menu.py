08a44c59b0629a7d1af50aed711f994dc568694e
AK47-Gen2005 <allenkarlbsisnorio2005@gmail.com>
2026-05-17 02:57:18 +0800

    0.3.0.3 Last Update [UI Improvements, Please test everything and check the codes]

--- TEXT_BASED_RPG_GAME/menu.py
+++ TEXT_BASED_RPG_GAME/menu.py
@@ -1,6 +1,72 @@
+from .logger import Logger
+import sys
+import time
+
+
+logger = Logger(delay=0.005)
+
+
+def enter_pressed():
+
+    if sys.platform.startswith("win"):
+
+        import msvcrt
+
+        if msvcrt.kbhit():
+
+            key = msvcrt.getwch()
+
+            if key == "\r":
+                return True
+
+        return False
+
+    else:
+
+        import select
+
+        readable, _, _ = select.select(
+            [sys.stdin],
+            [],
+            [],
+            0
+        )
+
+        if readable:
+            sys.stdin.readline()
+            return True
+
+        return False
+
+
+def log_block(text):
+
+    lines = text.splitlines()
+    skip = False
+
+    for line_index, line in enumerate(lines):
+
+        if skip:
+            print(line)
+            continue
+
+        for char_index, char in enumerate(line):
+
+            if enter_pressed():
+
+                print(line[char_index:], end="")
+                skip = True
+                break
+
+            print(char, end="", flush=True)
+            time.sleep(logger.delay)
+
+        print()
+
+
 def show_menu():
 
-    print(r"""
+    log_block(r"""
 =============================================================
                     EXILED BELOW
               A Text-Based Roguelike RPG
